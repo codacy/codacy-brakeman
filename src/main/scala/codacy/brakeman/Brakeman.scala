e98c09f6fd8d9a74ea0a6120db4c32efe67bbbc9
machado <machado@codacy.com>
2015-09-01 18:24:57 +0100

    added the warning codes to the patterns

--- src/main/scala/codacy/brakeman/Brakeman.scala
+++ src/main/scala/codacy/brakeman/Brakeman.scala
@@ -28,13 +28,6 @@ object Brakeman extends Tool {
 
     val command = getCommandFor(path, conf, files)
 
-    println("\n\nCMD:\n\n")
-
-    println(s"\n\n$command\n\n")
-
-
-    println("\n\nCMD:\n\n")
-
     val resultFromTool = command.lineStream(ProcessLogger(_ => ()))
 
     val results = parseToolResult(resultFromTool)
@@ -45,7 +38,71 @@ object Brakeman extends Tool {
   def warningToPatternId(warningCode: Int): String = {
     warningCode match {
       case 0 => "SQL"
+      case 1 => "SQL"
+      case 2 => "CrossSiteScripting"
+      case 6 => "ForgerySetting"
+      case 7 => "ForgerySetting"
+      case 8 => "SkipBeforeFilter"
+      case 9 => "BasicAuth"
+      case 10 => "SkipBeforeFilter"
+      case 11 => "DefaultRoutes"
+      case 12 => "DefaultRoutes"
+      case 13 => "Evaluation"
+      case 14 => "Execute"
+      case 15 => "Render"
+      case 16 => "FileAccess"
+      case 17 => "MassAssignment"
       case 18 => "Redirect"
+      case 19 => "ModelAttributes"
+      case 22 => "SelectVulnerability"
+      case 23 => "Send"
+      case 24 => "UnsafeReflection"
+      case 25 => "Deserialize"
+      case 26 => "SessionSettings"
+      case 27 => "SessionSettings"
+      case 28 => "TranslateBug"
+      case 29 => "SessionSettings"
+      case 30 => "ValidationRegex"
+      case 31 => "NestedAttributes"
+      case 32 => "MailTo"
+      case 33 => "ForgerySetting"
+      case 34 => "FilterSkipping"
+      case 35 => "QuoteTableName"
+      case 36 => "StripTags"
+      case 37 => "ResponseSplitting"
+      case 42 => "DigestDoS"
+      case 43 => "SelectTag"
+      case 44 => "SingleQuotes"
+      case 45 => "StripTags"
+      case 48 => "YAMLParsing"
+      case 49 => "JSONParsing"
+      case 50 => "ModelSerialize"
+      case 52 => "JSONParsing"
+      case 54 => "WithoutProtection"
+      case 55 => "SymbolDoSCVE"
+      case 57 => "JRubyXML"
+      case 59 => "SymbolDoS"
+      case 60 => "ModelAttrAccessible"
+      case 61 => "DetailedExceptions"
+      case 62 => "DetailedExceptions"
+      case 63 => "I18nXSS"
+      case 64 => "HeaderDoS"
+      case 70 => "MassAssignment"
+      case 71 => "SSLVerify"
+      case 72 => "SQLCVEs"
+      case 73 => "NumberToCurrency"
+      case 74 => "NumberToCurrency"
+      case 75 => "RenderDoS"
+      case 76 => "RegexDoS"
+      case 77 => "DefaultRoutes"
+      case 80 => "CreateWith"
+      case 81 => "CreateWith"
+      case 82 => "UnscopedFind"
+      case 83 => "EscapeFunction"
+      case 85 => "FileDisclosure"
+      case 86 => "ForgerySetting"
+      case 87 => "JSONEncoding"
+      case 88 => "XMLDoS"
       case _ => "Unknow Error"
     }
   }
